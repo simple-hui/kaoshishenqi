e0eb40aa15131ec9dc0ba3a44dba170a5d967db1
simple-hui <276557421@qq.com>
2019-12-09 17:35:59 +0800

    提交diamante

---
+++ src/components/allSzdl.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="allSzdl">
+		<div v-for="item in data.danxuan" :key="item.title">
+      <p>题目：{{ item.content }}</p>
+      <!-- <p>答案：{{ item.ans }}</p> -->
+      <p v-if="titleType === 'danxuan' || titleType === 'duoxuan'">
+        答案: <span v-for="items in item.ans.split('|')" class="resultContent">{{ item[items] }}<span>,</span></span>
+      </p>
+    </div>
+	</div>
+</template>
+<script>
+	import answer from '@/components/answer.js';
+	export default {
+		name: 'allSzdl',
+		data () {
+			return {
+				data: answer.mndl,
+				titleType: 'danxuan'
+			}
+		}
+	}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import home from '@/components/home'
+import allSzdl from '@/components/allSzdl'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: home
+    },
+    {
+    	path: '/allSzdl',
+    	name: 'allSzdl',
+    	component: allSzdl
     }
   ]
 })
